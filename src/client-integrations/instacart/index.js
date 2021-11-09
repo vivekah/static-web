a718ad71b8f6919f146b602c2c08570cbe9d0e0f
WF | Snezhana Dichevska <snezhana.dichevska@webfactory.mk>
2021-11-09 14:38:27 +0100

    Add personal impact widget

--- src/client-integrations/instacart/index.js
+++ src/client-integrations/instacart/index.js
@@ -2,3 +2,4 @@ export {default as NonprofitSelection} from './intacartNonprofitSelection';
 export {default as CommunityImpactIntegration} from './instacartImpact';
 export {default as NationalCommunityImpact} from './instacartNationalImpact';
 export {default as PostPurchaseView} from './instacatPostPurchase';
+export {default as PersonalImpact} from './instacartPersonalImpact';
---
+++ src/client-integrations/instacart/instacartPersonalImpact.js
@@ -0,0 +1,172 @@
+import * as components from "../../components";
+import {pathUtil} from "../../utils";
+
+window.execPersonalImpact = async function execPersonalImpact(apiKey,
+                                                              userId,
+                                                              fontFamily,
+                                                              lan = 'en',
+                                                              containerId,
+                                                              reviewResultsCallback = () => {
+                                                              }) {
+    const themeColorConfig = {
+    progressBarColor: '#16ad0b',
+    confirmationButtonColor: '#16ad0b',
+    causeTestColor: '#f0a358',
+    textColor: '#6a6b6d',
+    lightTextColor: '#bbbbbd',
+    progressBarBackgroundColor: '#e3e3e3'
+  }
+  const reviewResultsId = 'review-results-link-personal-impact';
+  await renderImpactScreen();
+
+  async function renderImpactScreen() {
+    const impactData = await getImpactData(userId);
+
+    let impactScreenContainer = new components.BeamContainer({
+      style: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      },
+      children: [
+        getJoinUsSection(impactData),
+      ]
+    });
+    let container = document.getElementById(containerId);
+    if (container) {
+      container.append(impactScreenContainer.view);
+    } else {
+      document.body.append(impactScreenContainer.view);
+    }
+    addCallbacks();
+  }
+
+  function addCallbacks() {
+    document.getElementById(reviewResultsId).addEventListener('click', reviewResultsCallback);
+  }
+
+  async function getImpactData(userId) {
+    const beamWebSdkBaseUrl = process.env.BEAM_BACKEND_BASE_URL;
+    let fullUrl = new URL('api/v2/users/personal-impact/instacart', beamWebSdkBaseUrl);
+    const params = {
+      partner_user_id: userId,
+      lan: lan
+    }
+    if (params)
+      fullUrl.search = new URLSearchParams(params)
+        .toString()
+        .replace(/null/g, "")
+        .replace(/undefined/g, "");
+    try {
+      let response = await window.fetch(fullUrl, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Api-Key ${apiKey}`
+        }
+      });
+      if (response.status == 200) return await response.json();
+    } catch (err) {
+      console.error(err);
+    }
+    return null;
+  }
+
+  function getJoinUsSection(impactData) {
+    return new components.BeamFlexWrapper({
+      alignItems: 'flex-start',
+      style: {
+        maxWidth: '600px',
+        height: '100px',
+        justifyContent: 'center',
+        margin: 'auto',
+        marginTop: '0px',
+        flexWrap: 'nowrap !important',
+        padding: '0px 20px',
+        fontFamily: fontFamily || 'inherit'
+      },
+      children: [
+        new components.BeamFlexWrapper({
+          children: [
+            new components.BeamImage({
+              alt: 'Instacart Purchase Icon',
+              src: impactData?.image || pathUtil.getAsset('instacart_purchase_icon.png'),
+              style: {
+                borderRadius: '50%',
+                width: '60px',
+                height: '60px',
+                display: 'flex',
+                margin: '0 10px 0 10px'
+              }
+            })
+          ]
+        }),
+        new components.BeamFlexWrapper({
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          children: [
+            new components.BeamText({
+              text: impactData?.header || 'Join us in the fight against food insecurity',
+              style: {
+                fontFamily: fontFamily || 'inherit',
+                fontSize: '18px',
+                lineHeight: '26px',
+                fontWeight: '600'
+              }
+            }),
+            new components.BeamText({
+              text: `${impactData?.description || 'Food meals this holiday season by simply placing your order.'}`,
+              style: {
+                fontSize: '12px',
+                lineHeight: '18px',
+                fontFamily: fontFamily || 'inherit'
+              }
+            }),
+            new components.BeamFlexWrapper({
+              noWrap: true,
+              width: '100%',
+              alignItems: 'center',
+              alignContent: 'center',
+              children: [
+                impactData?.percentage && new components.BeamProgressWrapper({
+                  percentage: impactData?.percentage,
+                  height: "4px",
+                  backgroundColor: themeColorConfig.progressBarBackgroundColor,
+                  border: "none",
+                  cornerRadius: undefined,
+                  style: {
+                    width: '90%',
+                    height: '4px',
+                    display: 'flex',
+                    marginTop: '0px',
+                    marginRight: '12px',
+                    fontFamily: fontFamily || 'inherit'
+                  }
+                }),
+                // percent text
+                impactData?.percentage && new components.BeamText({
+                  tag: "h6",
+                  text: impactData?.percentage + "&#37;",
+                  fontSize: '12px',
+                  color: '#343538',
+                  fontWeight: '200',
+                  fontFamily: fontFamily || 'inherit'
+                })]
+            }),
+            new components.BeamText({
+              style: {
+                fontSize: '12px',
+                lineHeight: '18px',
+                marginTop: '11px',
+                fontFamily: fontFamily || 'inherit'
+              },
+              text: `<a href='#' id="${reviewResultsId}" style='color: green; text-decoration: none;'>${impactData?.cta} </a>`
+            })
+          ]
+        }),
+      ]
+    });
+  }
+
+}
+export default window.execPersonalImpact;
